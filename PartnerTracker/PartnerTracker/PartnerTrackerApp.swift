ebce8c4cdb0be1c2f01cdb8e396e7dd7732f2613
ArturGuenter <152178843+ArturGuenter@users.noreply.github.com>
2025-06-16 21:51:34 +0200

    Add files via upload
    init

---
+++ PartnerTracker/PartnerTracker/PartnerTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartnerTrackerApp.swift
+//  PartnerTracker
+//
+//  Created by Artur Günter on 13.06.25.
+//
+
+import SwiftUI
+
+@main
+struct PartnerTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PartnerTracker/PartnerTrackerTests/PartnerTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PartnerTrackerTests.swift
+//  PartnerTrackerTests
+//
+//  Created by Artur Günter on 13.06.25.
+//
+
+import Testing
+@testable import PartnerTracker
+
+struct PartnerTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
