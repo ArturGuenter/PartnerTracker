fc8cfb3b6b8ba54bb51c77aefdceb21b4eb34b65
arturguenter <artur.guenter@icloud.com>
2025-06-17 22:19:33 +0200

    - viewmodel für login und registrierung

---
+++ PartnerTracker/PartnerTracker/MVVM/LoginRegisterViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  LoginRegisterViewModel.swift
+//  PartnerTracker
+//
+//  Created by Artur Günter on 17.06.25.
+//
+
+import Foundation
+class LoginRegisterViewModel: ObservableObject {
+    
+}
