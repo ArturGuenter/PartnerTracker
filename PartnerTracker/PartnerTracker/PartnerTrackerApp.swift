0bfdc6eff63c57af4b8707a836dfe569c0c44446
arturguenter <artur.guenter@icloud.com>
2025-06-18 17:19:00 +0200

    - firebase im init angelegt

--- PartnerTracker/PartnerTracker/PartnerTrackerApp.swift
+++ PartnerTracker/PartnerTracker/PartnerTrackerApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PartnerTrackerApp: App {
+    
+    init(){
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
