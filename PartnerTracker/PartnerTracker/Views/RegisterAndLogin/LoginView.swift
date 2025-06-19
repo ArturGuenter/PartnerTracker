c0114346a14ce5c3dd20433e83b0f520f37f0861
arturguenter <artur.guenter@icloud.com>
2025-06-19 21:52:32 +0200

    - login view bearbeiten

--- PartnerTracker/PartnerTracker/Views/RegisterAndLogin/LoginView.swift
+++ PartnerTracker/PartnerTracker/Views/RegisterAndLogin/LoginView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @ObservedObject var loginRegisterViewModel: LoginRegisterViewModel
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    LoginView()
+    LoginView(loginRegisterViewModel: LoginRegisterViewModel())
 }
