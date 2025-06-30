95db91211acac12ebd50f5af80304cdd61f686aa
arturguenter <artur.guenter@icloud.com>
2025-06-30 19:35:59 +0200

    - navigation bug gefixt

--- PartnerTracker/PartnerTracker/Views/RegisterAndLogin/LoginView.swift
+++ PartnerTracker/PartnerTracker/Views/RegisterAndLogin/LoginView.swift
@@ -14,63 +14,65 @@ struct LoginView: View {
     @State private var password = ""
     
     var body: some View {
-        VStack(spacing: 24) {
-            HStack{
-                Spacer()
-                
-                Text("Login")
-                    .font(.largeTitle.bold())
-                    .foregroundColor(.primary)
-                    .frame(maxWidth: .infinity)
-                
-                Spacer()
-            }
-
-                    TextField("E-Mail", text: $email)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(12)
-                        .overlay(
-                                                    RoundedRectangle(cornerRadius: 12)
-                                                        .stroke(Color.gray, lineWidth: 1)
-                                                )
-                        
-            
-            
-            
-                    SecureField("Passwort", text: $password)
-                        .textFieldStyle(PlainTextFieldStyle())
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(12)
-                        .overlay(
-                                                    RoundedRectangle(cornerRadius: 12)
-                                                        .stroke(Color.gray, lineWidth: 1)
-                        )
-
-                    Button(action: {
-                        loginRegisterViewModel.login(email: email, password: password)
-                    }) {
-                        Text("Einloggen")
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(12)
-                    }
-            
-            NavigationLink(destination: RegisterView(loginRegisterViewModel: loginRegisterViewModel)){
-                Text("Registrieren")
-                    .font(.headline)
-                    .foregroundStyle(.blue)
-            }
-
+        NavigationStack{
+            VStack(spacing: 24) {
+                HStack{
+                    Spacer()
+                    
+                    Text("Login")
+                        .font(.largeTitle.bold())
+                        .foregroundColor(.primary)
+                        .frame(maxWidth: .infinity)
+                    
                     Spacer()
                 }
-                .padding()
-                .background(Color(.systemGroupedBackground))
+                
+                TextField("E-Mail", text: $email)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(12)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                
+                
+                
+                
+                SecureField("Passwort", text: $password)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(12)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                
+                Button(action: {
+                    loginRegisterViewModel.login(email: email, password: password)
+                }) {
+                    Text("Einloggen")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(12)
+                }
+                
+                NavigationLink(destination: RegisterView(loginRegisterViewModel: loginRegisterViewModel)){
+                    Text("Registrieren")
+                        .font(.callout)
+                        .foregroundStyle(.blue)
+                }
+                
+                Spacer()
             }
+            .padding()
+            .background(Color(.systemGroupedBackground))
+        }
+    }
     
 }
 
