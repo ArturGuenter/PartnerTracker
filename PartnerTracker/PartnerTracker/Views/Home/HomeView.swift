43c4d86d1188a17c63b750e65317a009fec7a36a
arturguenter <artur.guenter@icloud.com>
2025-07-02 13:03:33 +0200

    - debugging login signout ,signout Button hinzugefügt

--- PartnerTracker/PartnerTracker/Views/Home/HomeView.swift
+++ PartnerTracker/PartnerTracker/Views/Home/HomeView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+
 struct HomeView: View {
-    
     @Binding var selection: Int
+    @ObservedObject var loginRegisterViewModel: LoginRegisterViewModel
     
     var body: some View {
-        Text("Home")
+        VStack(spacing: 24) {
+            Text("Willkommen, \(loginRegisterViewModel.user?.name ?? "Benutzer")!")
+                .font(.title2)
+            
+            Button(role: .destructive) {
+                loginRegisterViewModel.signOut()
+            } label: {
+                Text("Abmelden")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+                    .cornerRadius(12)
+            }
+
+            Spacer()
+        }
+        .padding()
     }
 }
 
+
 #Preview {
-    HomeView(selection: .constant(1))
+    HomeView(selection: .constant(1), loginRegisterViewModel: LoginRegisterViewModel())
 }
