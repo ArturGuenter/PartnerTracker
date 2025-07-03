691103b41b412a1b2176c4aecfd0b06b557fb4bc
arturguenter <artur.guenter@icloud.com>
2025-07-03 10:40:53 +0200

    - debugging taskview

--- PartnerTracker/PartnerTracker/Views/Task/TaskView.swift
+++ PartnerTracker/PartnerTracker/Views/Task/TaskView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct TaskView: View {
+    @ObservedObject var taskViewModel: TaskViewModel
+    @ObservedObject var groupViewModel: GroupViewModel
+
     var body: some View {
-        Text("Task")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+
+                // Eigene Aufgaben
+                if !taskViewModel.personalTasks.isEmpty {
+                    Text("Meine Aufgaben")
+                        .font(.headline)
+
+                    ForEach(taskViewModel.personalTasks) { task in
+                        TaskRow(task: task)
+                    }
+                }
+
+                // Gruppenaufgaben
+                ForEach(Array(taskViewModel.groupedTasks.keys), id: \.self) { groupName in
+                    if let tasks = taskViewModel.groupedTasks[groupName] {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Gruppe: \(groupName)")
+                                .font(.headline)
+
+                            ForEach(tasks) { task in
+                                TaskRow(task: task)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+        }
+        .onAppear {
+            Task {
+                try? await taskViewModel.fetchTasks(groups: groupViewModel.groups)
+            }
+        }
     }
 }
+
 
 #Preview {
     TaskView()
