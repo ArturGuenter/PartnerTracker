eaeaaddde4ee1f6aa1d2a8c8dd09fee9354aaf54
arturguenter <artur.guenter@icloud.com>
2025-07-14 12:23:17 +0200

    - view sheet zum bearbeiten states erstellen

--- PartnerTracker/PartnerTracker/Views/Task/SubView/EditTaskSheet.swift
+++ PartnerTracker/PartnerTracker/Views/Task/SubView/EditTaskSheet.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct EditTaskSheet: View {
+    
+    @State var task: TaskItem
+    @State private var updatedTitle: String
+        var onSave: (String) -> Void
+
+        init(task: TaskItem, onSave: @escaping (String) -> Void) {
+            self.task = task
+            self.onSave = onSave
+            _updatedTitle = State(initialValue: task.title)
+        }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    EditTaskSheet()
+    EditTaskSheet(task: <#TaskItem#>, onSave: <#(String) -> Void#>)
 }
