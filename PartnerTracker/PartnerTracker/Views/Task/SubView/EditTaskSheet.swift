c799c9f6c285cccb289f85108d478037030031f8
arturguenter <artur.guenter@icloud.com>
2025-07-14 12:22:02 +0200

    - view für sheet erstellt fürs bearbeiten der Tasks

---
+++ PartnerTracker/PartnerTracker/Views/Task/SubView/EditTaskSheet.swift
@@ -0,0 +1,18 @@
+//
+//  EditTaskSheet.swift
+//  PartnerTracker
+//
+//  Created by Artur Günter on 14.07.25.
+//
+
+import SwiftUI
+
+struct EditTaskSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditTaskSheet()
+}
