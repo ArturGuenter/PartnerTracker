8cf851d2c0b5e531231f9b777a8264bfe40cad66
arturguenter <artur.guenter@icloud.com>
2025-07-18 19:40:00 +0200

    - bug mit zu vielen sheets debugging

---
+++ PartnerTracker/PartnerTracker/Enum/TaskSheetType.swift
@@ -0,0 +1,25 @@
+//
+//  TaskSheetType.swift
+//  PartnerTracker
+//
+//  Created by Artur Günter on 18.07.25.
+//
+
+import Foundation
+
+enum TaskSheetType: Identifiable {
+    case personal
+    case group(Group)
+    case edit(TaskItem)
+
+    var id: String {
+        switch self {
+        case .personal:
+            return "personal"
+        case .group(let group):
+            return "group_\(group.id)"
+        case .edit(let task):
+            return "edit_\(task.id)"
+        }
+    }
+}
