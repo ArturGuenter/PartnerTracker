69740912fe1cea11efeff9afe41fcbd64fc77661
ArturGuenter <artur.guenter@icloud.com>
2025-09-06 22:10:27 +0200

    - funktion um den status der gruppen bei änderung zu checken

--- PartnerTracker/PartnerTracker/MVVM/NotificationViewModel.swift
+++ PartnerTracker/PartnerTracker/MVVM/NotificationViewModel.swift
@@ -12,4 +12,30 @@ import UserNotifications
 class NotificationViewModel: ObservableObject {
     private var debounceTimers: [String: Task<Void, Never>] = [:]
     
+    init() {
+            requestAuthorization()
+        }
+
+        
+        func handleGroupStatusChange(group: Group, tasks: [TaskItem]) {
+            let allDone = tasks.allSatisfy { !$0.completedBy.isEmpty }
+
+            if allDone {
+                debounceTimers[group.id]?.cancel()
+                debounceTimers[group.id] = Task {
+                    try? await Task.sleep(nanoseconds: 10 * 1_000_000_000)
+
+                    
+                    let stillAllDone = tasks.allSatisfy { !$0.completedBy.isEmpty }
+                    if stillAllDone {
+                        await self.sendLocalNotification(for: group)
+                    }
+                }
+            } else {
+                
+                debounceTimers[group.id]?.cancel()
+                debounceTimers[group.id] = nil
+            }
+        }
+    
 }
