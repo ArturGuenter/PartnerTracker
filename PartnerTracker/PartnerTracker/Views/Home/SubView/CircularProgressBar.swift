c1fc715ba8f621859e9998e8d20c452638dee33d
ArturGuenter <artur.guenter@icloud.com>
2025-09-10 13:20:18 +0200

    - farben angepasst

--- PartnerTracker/PartnerTracker/Views/Home/SubView/CircularProgressBar.swift
+++ PartnerTracker/PartnerTracker/Views/Home/SubView/CircularProgressBar.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct CircularProgressBar: View {
     var progress: Double
     var title: String
@@ -16,39 +14,35 @@ struct CircularProgressBar: View {
     var total: Int
     var size: CGFloat
     var progressColor: Color = .blue
-    
+
     var body: some View {
-        VStack {
-            ZStack {
-                Circle()
-                    .stroke(lineWidth: 15)
-                    .foregroundColor(.gray.opacity(0.2))
-
-                
-                Circle()
-                    .trim(from: 0.0, to: CGFloat(min(progress, 1.0)))
-                    .stroke(style: StrokeStyle(lineWidth: 15, lineCap: .round))
-                    .foregroundColor(progressColor)  // volle Farbe
-                    .rotationEffect(Angle(degrees: -90))
-                    .animation(.easeOut, value: progress)
-
-                
-                VStack {
-                    Text("\(Int(progress * 100))%")
-                        .font(.title2)
-                        .bold()
-                    Text("\(completed)/\(total)")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                }
-            }
-            .frame(width: size, height: size)
+        ZStack {
             
-            Text(title)
-                .font(.headline)
+            Circle()
+                .stroke(lineWidth: 15)
+                .foregroundColor(progressColor.opacity(0.2))
+
+            
+            Circle()
+                .trim(from: 0.0, to: CGFloat(min(progress, 1.0)))
+                .stroke(style: StrokeStyle(lineWidth: 15, lineCap: .round))
+                .foregroundColor(progressColor)
+                .rotationEffect(Angle(degrees: -90))
+                .animation(.easeOut, value: progress)
+
+            VStack {
+                Text(title)
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                Text("\(completed)/\(total)")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
         }
+        .frame(width: size, height: size)
     }
 }
+
 
 
 
